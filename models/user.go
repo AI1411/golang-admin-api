9ed548b385983bf27aabb37aa7e8751552aab8e6
AI1411 <goethe0719@gmail.com>
2022-02-26 09:33:37 +0900

    model/userにImage追加

--- models/user.go
+++ models/user.go
@@ -6,6 +6,7 @@ type User struct {
 	ID        int       `json:"id" gorm:"primaryKey"`
 	FirstName string    `json:"first_name"`
 	LastName  string    `json:"last_name"`
+	Image     string    `json:"image"`
 	Age       uint8     `json:"age"`
 	Email     string    `json:"email"`
 	Password  string    `json:"password"`
