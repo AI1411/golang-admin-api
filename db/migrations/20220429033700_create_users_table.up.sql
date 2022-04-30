4070c8c1450bcce0a48e9c916b2443f85db9df2a
AI1411 <goethe0719@gmail.com>
2022-04-30 10:49:25 +0900

    DB定義修正

--- db/migrations/20220429033700_create_users_table.up.sql
+++ db/migrations/20220429033700_create_users_table.up.sql
@@ -1,14 +1,14 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users
 (
-    id         INT UNSIGNED primary key NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(64)              NOT NULL,
-    last_name  VARCHAR(64)              NOT NULL,
-    age        integer unsigned         NOT NULL,
-    email      VARCHAR(64)              NOT NULL,
-    password   VARCHAR(255)             NOT NULL,
-    created_at DATETIME                 NOT NULL,
-    updated_at DATETIME                 NOT NULL,
+    id         int primary key auto_increment     NOT NULL comment 'ID',
+    first_name VARCHAR(64)                        NOT NULL comment '名前',
+    last_name  VARCHAR(64)                        NOT NULL comment '姓',
+    age        integer unsigned                   NOT NULL comment '年齢',
+    email      VARCHAR(64)                        NOT NULL comment 'メールアドレス',
+    password   VARCHAR(255)                       NOT NULL comment 'パスワード',
+    created_at DATETIME default current_timestamp NOT NULL comment '作成日時',
+    updated_at DATETIME default current_timestamp NOT NULL comment '更新日時',
     KEY index_users_on_email (email),
     KEY index_users_on_first_name_and_last_name (first_name, last_name)
 ) ENGINE = InnoDB
--- db/migrations/20220429033706_create_todos_table.up.sql
+++ db/migrations/20220429033706_create_todos_table.up.sql
@@ -1,13 +1,13 @@
 DROP TABLE IF EXISTS todos;
 CREATE TABLE todos
 (
-    id         INT UNSIGNED primary key NOT NULL AUTO_INCREMENT,
-    title      VARCHAR(64)              NOT NULL,
-    body       TEXT                     NOT NULL,
-    status     varchar(64)              NOT NULL,
-    user_id    INT UNSIGNED             NOT NULL,
-    created_at DATETIME                 NOT NULL,
-    updated_at DATETIME                 NOT NULL,
+    id         INT UNSIGNED primary key           NOT NULL AUTO_INCREMENT,
+    title      VARCHAR(64)                        NOT NULL comment 'タイトル',
+    body       TEXT                               NOT NULL comment '本文',
+    status     varchar(64)                        NOT NULL comment 'ステータス',
+    user_id    INT UNSIGNED                       NOT NULL comment 'ユーザーID',
+    created_at DATETIME default current_timestamp NOT NULL comment '作成日時',
+    updated_at DATETIME default current_timestamp NOT NULL comment '更新日時',
     KEY user_id_idx (user_id),
     KEY title_idx (title),
     KEY status_idx (status)
