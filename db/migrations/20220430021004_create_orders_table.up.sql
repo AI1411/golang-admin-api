682c000cb98336ef7861b12abad63722c067cd95
AI1411 <goethe0719@gmail.com>
2022-05-14 10:31:11 +0900

    add column and index

--- db/migrations/20220430021004_create_orders_table.up.sql
+++ db/migrations/20220430021004_create_orders_table.up.sql
@@ -10,7 +10,8 @@ CREATE TABLE orders
     created_at   timestamp default current_timestamp NOT NULL comment '作成日時',
     updated_at   timestamp default current_timestamp NOT NULL comment '更新日時',
     KEY index_products_on_id (id),
-    KEY index_products_on_user_id (user_id)
+    KEY index_products_on_user_id (user_id),
+    KEY index_products_on_order_status (order_status)
 ) ENGINE = InnoDB
   DEFAULT character set = 'utf8mb4'
   collate = 'utf8mb4_general_ci' COMMENT = '注文テーブル';
--- db/migrations/20220430025734_create_order_details_table.up.sql
+++ db/migrations/20220430025734_create_order_details_table.up.sql
@@ -1,16 +1,18 @@
 DROP TABLE IF EXISTS order_details;
 CREATE TABLE order_details
 (
-    id         char(36) primary key                NOT NULL comment '注文詳細ID',
-    order_id   char(36)                            NOT NULL comment '注文ID',
-    product_id char(36)                            NOT NULL comment '商品ID',
-    quantity   varchar(64)                         NOT NULL comment '数量',
-    price      integer unsigned                    NOT NULL comment '価格',
-    created_at timestamp default current_timestamp NOT NULL comment '作成日時',
-    updated_at timestamp default current_timestamp NOT NULL comment '更新日時',
-    KEY index_products_on_id (id),
-    KEY index_products_on_order_id (order_id),
-    KEY index_products_on_product_id (product_id)
+    id                  char(36) primary key                NOT NULL comment '注文詳細ID',
+    order_id            char(36)                            NOT NULL comment '注文ID',
+    product_id          char(36)                            NOT NULL comment '商品ID',
+    quantity            varchar(64)                         NOT NULL comment '数量',
+    price               integer unsigned                    NOT NULL comment '価格',
+    order_detail_status varchar(64)                         NOT NULL comment '注文詳細ステータス',
+    created_at          timestamp default current_timestamp NOT NULL comment '作成日時',
+    updated_at          timestamp default current_timestamp NOT NULL comment '更新日時',
+    KEY index_order_details_on_id (id),
+    KEY index_order_details_on_order_id (order_id),
+    KEY index_order_details_on_product_id (product_id),
+    KEY index_order_details_on_order_detail_status (order_detail_status)
 ) ENGINE = InnoDB
   DEFAULT character set = 'utf8mb4'
   collate = 'utf8mb4_general_ci' COMMENT = '注文詳細テーブル';
