d6489cdc7a0f8aeb17a3b0f7a35fdd9017e12097
AI1411 <goethe0719@gmail.com>
2022-05-14 17:06:30 +0900

    Csv出力バッチ

---
+++ batch/export_order_list.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"encoding/csv"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/urfave/cli/v2"
+
+	"github.com/AI1411/golang-admin-api/db"
+	"github.com/AI1411/golang-admin-api/models"
+)
+
+func export(args []string) error {
+	app := &cli.App{
+		Name:  "注文一覧をCSVにエクスポートする",
+		Usage: "本日分の注文一覧をCSVにエクスポートする",
+		Action: func(c *cli.Context) error {
+			year, month, day := time.Now().Date()
+			dateStr := fmt.Sprintf("%d%02d%02d", year, month, day)
+			fileName := dateStr + "_orders.csv"
+			fileDir := "assets/csv/orders/" + strconv.Itoa(year) +
+				"/" + strconv.Itoa(int(month)) + "/" + strconv.Itoa(day) + "/"
+
+			checkDir(fileDir)
+
+			filePath := fileDir + fileName
+
+			if err := createCsvFile(filePath); err != nil {
+				return err
+			}
+
+			return nil
+		},
+	}
+
+	err := app.Run(args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
+}
+
+func checkDir(fileDir string) error {
+	_, err := os.Stat(fileDir)
+	if os.IsNotExist(err) {
+		os.MkdirAll(fileDir, 0o777)
+		return errors.New("ディレクトリを作成しました。")
+	}
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func createCsvFile(filePath string) error {
+	f, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	writer := csv.NewWriter(f)
+	defer writer.Flush()
+
+	var orders []models.Order
+	dbConn := db.Init()
+
+	if err = dbConn.Find(&orders).Error; err != nil {
+		return err
+	}
+
+	if err := writer.Write([]string{
+		"注文ID",
+		"ユーザID",
+		"数量",
+		"合計金額",
+		"注文ステータス",
+		"注文備考",
+		"作成日時",
+		"更新日時",
+	}); err != nil {
+		return err
+	}
+
+	for _, order := range orders {
+		if err := writer.Write([]string{
+			order.ID,
+			strconv.Itoa(int(order.UserID)),
+			strconv.Itoa(int(order.Quantity)),
+			strconv.Itoa(int(order.TotalPrice)),
+			string(order.OrderStatus),
+			order.Remarks,
+			order.CreatedAt.Format("2006-01-02 15:04:05"),
+			order.UpdatedAt.Format("2006-01-02 15:04:05"),
+		}); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	fmt.Println("注文一覧CSVエクスポートを開始します。")
+	if err := export(os.Args); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("注文一覧CSVエクスポート終了しました。")
+}
--- util/errors/rest_errors.go
+++ util/errors/rest_errors.go
@@ -77,3 +77,11 @@ func NewInternalServerError(message string, err error) RestErr {
 func Is(err error, target error) bool {
 	return errors.Is(err, target)
 }
+
+func As(err error, target interface{}) bool {
+	return errors.As(err, &target)
+}
+
+func New(text string) error {
+	return errors.New(text)
+}
