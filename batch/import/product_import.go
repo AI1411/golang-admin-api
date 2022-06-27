a5c86245efec1e8c642cfe678c910609d1dca95f
AI1411 <goethe0719@gmail.com>
2022-06-28 07:35:38 +0900

    csv import

---
+++ batch/import/product_import.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+
+	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/transform"
+)
+
+func import_product_from_csv() error {
+	f, err := os.Open("33OKAYAM 2.CSV")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	r := csv.NewReader(transform.NewReader(f, japanese.ShiftJIS.NewDecoder()))
+	for {
+		records, err := r.Read()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(records)
+	}
+}
+
+func main() {
+	fmt.Println("商品一覧CSVインポートを開始します。")
+	if err := import_product_from_csv(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("商品一覧CSVインポートを開始します。")
+}
