34dff542565a5302ef35c90fe12fa64e46d4a7dc
AI1411 <goethe0719@gmail.com>
2022-06-28 08:32:58 +0900

    uuid生成バッチ

---
+++ batch/uuid_maker/uuid_maker.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/urfave/cli/v2"
+	"log"
+	"os"
+	"strconv"
+)
+
+func makeUUID() error {
+	app := &cli.App{
+		Name: "UUIDを引数分生成する",
+		Action: func(c *cli.Context) error {
+			n, _ := strconv.Atoi(c.Args().Get(0))
+			for i := 0; i < n; i++ {
+				newUuid, _ := uuid.NewRandom()
+				fmt.Println(newUuid.String())
+			}
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
+}
+
+func main() {
+	fmt.Println("uuid_makerを開始します。")
+	if err := makeUUID(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("uuid_makerを終了します。")
+}
