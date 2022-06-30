26e4c6c092fe9ca333d613f1956401d83ee62c3d
AI1411 <goethe0719@gmail.com>
2022-07-01 07:34:03 +0900

    output log file

--- server/logger.go
+++ server/logger.go
@@ -3,6 +3,7 @@ package logger
 import (
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"time"
 )
 
 func NewLogger(debug bool) (*zap.Logger, error) {
@@ -14,12 +15,14 @@ func NewLogger(debug bool) (*zap.Logger, error) {
 	encoderConfig := zap.NewProductionEncoderConfig()
 	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
 
+	today := time.Now().Format("2006-01-02")
+	outputFileName := "log/" + today + ".log"
 	return zap.Config{
 		Level:            zap.NewAtomicLevelAt(logLevel),
 		Development:      debug,
 		Encoding:         "json",
 		EncoderConfig:    encoderConfig,
-		OutputPaths:      []string{"stdout"},
+		OutputPaths:      []string{"stdout", outputFileName},
 		ErrorOutputPaths: []string{"stderr"},
 	}.Build()
 }
