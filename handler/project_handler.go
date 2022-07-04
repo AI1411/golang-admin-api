0300300fa2f95fed3e6771381ba29bc3026343de
AI1411 <goethe0719@gmail.com>
2022-07-05 07:10:45 +0900

    add swagger document of order projects

--- handler/project_handler.go
+++ handler/project_handler.go
@@ -34,6 +34,36 @@ type searchProjectParams struct {
 	Limit        string `form:"limit,default=10" binding:"omitempty,numeric"`
 }
 
+type projectRequest struct {
+	ProjectTitle       string `json:"project_title" binding:"required,max=64" example:"project title"`
+	ProjectDescription string `json:"project_description" binding:"required,max=255" example:"project description"`
+}
+
+type projectResponseItem struct {
+	Id                 string `json:"id" example:"218c51c0-904e-4743-a2ae-94f0e34a0d6f"`
+	ProjectTitle       string `json:"project_title" example:"218c51c0-904e-4743-a2ae-94f0e34a0d6f"`
+	ProjectDescription string `json:"project_description" example:"description"`
+}
+
+type projectsResponse struct {
+	Total    int              `json:"total"`
+	Projects []models.Project `json:"projects"`
+}
+
+// GetProjects @title 一覧取得
+// @id GetProjects
+// @tags projects
+// @version バージョン(1.0)
+// @description 指定された条件に一致するproject一覧情報を取得する
+// @Summary project一覧取得
+// @Produce json
+// @Success 200 {object} projectsResponse
+// @Failure 400 {object} errorResponse
+// @Failure 500 {object} errorResponse
+// @Router /projects [GET]
+// @Param project_title query string false "タイトル" maxlength(64)
+// @Param offset query int false "開始位置" default(0) minimum(0)
+// @Param limit query int false "取得上限" default(12) minimum(1) maximum(100)
 func (h *ProjectHandler) GetProjects(ctx *gin.Context) {
 	traceID := appcontext.GetTraceID(ctx)
 	var params searchProjectParams
@@ -55,6 +85,19 @@ func (h *ProjectHandler) GetProjects(ctx *gin.Context) {
 	})
 }
 
+// GetProjectDetail @title project詳細
+// @id GetProjectDetail
+// @tags projects
+// @version バージョン(1.0)
+// @description project詳細を返す
+// @Summary project詳細取得
+// @Produce json
+// @Success 200 {object} projectResponseItem
+// @Failure 400 {object} errorResponse
+// @Failure 404 {object} errorResponse
+// @Failure 500 {object} errorResponse
+// @Router /projects/:id [GET]
+// @Param id path string true "ID" minlength(36) maxlength(36) format(UUID v4)
 func (h *ProjectHandler) GetProjectDetail(ctx *gin.Context) {
 	id := ctx.Param("id")
 	var project models.Project
@@ -70,6 +113,19 @@ func (h *ProjectHandler) GetProjectDetail(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, project)
 }
 
+// CreateProject @title project作成
+// @id CreateProject
+// @tags projects
+// @version バージョン(1.0)
+// @description projectを作成する
+// @Summary project作成
+// @Produce json
+// @Success 201 {object} projectResponseItem
+// @Failure 400 {object} errorResponse
+// @Failure 500 {object} errorResponse
+// @Router /projects [POST]
+// @Accept json
+// @Param projectRequest body projectRequest true "create project"
 func (h *ProjectHandler) CreateProject(ctx *gin.Context) {
 	traceID := appcontext.GetTraceID(ctx)
 	var project models.Project
@@ -87,6 +143,20 @@ func (h *ProjectHandler) CreateProject(ctx *gin.Context) {
 	ctx.JSON(201, project)
 }
 
+// UpdateProject @title project編集
+// @id UpdateProject
+// @tags projects
+// @version バージョン(1.0)
+// @description projectを編集する
+// @Summary project編集
+// @Produce json
+// @Success 202 {object} projectResponseItem
+// @Failure 400 {object} errorResponse
+// @Failure 500 {object} errorResponse
+// @Router /projects/:id [PUT]
+// @Accept json
+// @Param projectRequest body projectRequest true "update project"
+// @Param id path string true "ID" minlength(36) maxlength(36) format(UUID v4)
 func (h *ProjectHandler) UpdateProject(ctx *gin.Context) {
 	var project models.Project
 	id := ctx.Param("id")
@@ -115,6 +185,19 @@ func (h *ProjectHandler) UpdateProject(ctx *gin.Context) {
 	ctx.JSON(http.StatusAccepted, project)
 }
 
+// DeleteProject @title project削除
+// @id DeleteProject
+// @tags projects
+// @version バージョン(1.0)
+// @description projectを削除する
+// @Summary project削除
+// @Produce json
+// @Success 204
+// @Failure 400 {object} errorResponse
+// @Failure 500 {object} errorResponse
+// @Router /projects/:id [DELETE]
+// @Accept json
+// @Param id path string true "ID" minlength(36) maxlength(36) format(UUID v4)
 func (h *ProjectHandler) DeleteProject(ctx *gin.Context) {
 	project := models.Project{}
 	id := ctx.Param("id")
