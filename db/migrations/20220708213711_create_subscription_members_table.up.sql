f8cb44282d2f18b7c3c48760321c2c3eee68c651
AI1411 <goethe0719@gmail.com>
2022-07-12 07:40:56 +0900

    wip

--- db/migrations/20220708213711_create_subscription_members_table.up.sql
+++ db/migrations/20220708213711_create_subscription_members_table.up.sql
@@ -1,13 +1,13 @@
 DROP TABLE IF EXISTS `subscription_members`;
 CREATE TABLE `subscription_members`
 (
-    id                     char(36)                            NOT NULL comment 'ID',
-    user_id                char(36)                            NOT NULL comment 'ユーザID',
-    member_status          varchar(64)                         NOT NULL comment '会員ステータス',
-    member_start_date      timestamp default current_timestamp NOT NULL comment '会員開始日',
-    member_end_date        timestamp                           NOT NULL comment '会員開始日',
-    created_at             timestamp default current_timestamp NOT NULL comment '作成日時',
-    updated_at             timestamp default current_timestamp NOT NULL comment '更新日時'
+    id                char(36)                            NOT NULL comment 'ID',
+    user_id           char(36)                            NOT NULL comment 'ユーザID',
+    member_status     varchar(64)                         NOT NULL comment '会員ステータス',
+    member_start_date timestamp default current_timestamp NOT NULL comment '会員開始日',
+    member_end_date   timestamp default current_timestamp NOT NULL comment '会員開始日',
+    created_at        timestamp default current_timestamp NOT NULL comment '作成日時',
+    updated_at        timestamp default current_timestamp NOT NULL comment '更新日時'
 ) ENGINE = InnoDB
   DEFAULT character
       set = 'utf8mb4'
